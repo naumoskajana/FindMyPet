dde1ac52946ef2ee4df9c87a7ecd8cae91f49b93
jana.naumoska <jana.naumoska@scalefocus.com>
2023-06-28 12:31:03 +0200

    Changed NotificationController mapping

--- src/main/java/com/example/findmypet/web/NotificationController.java
+++ src/main/java/com/example/findmypet/web/NotificationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/lost-pets")
+@RequestMapping("/api/notifications")
 public class NotificationController {
 
     private final NotificationService notificationService;
