4b0ebe8b8359d526a20c8b7c7a0a4cd3b12ba90d
jana.naumoska <jana.naumoska@scalefocus.com>
2023-07-05 13:53:47 +0200

    Fixed endpoint for retrieving photo

--- src/main/java/com/example/findmypet/web/PhotoController.java
+++ src/main/java/com/example/findmypet/web/PhotoController.java
@@ -1,6 +1,5 @@
 package com.example.findmypet.web;
 
-import com.example.findmypet.dto.PhotoGetDTO;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -8,33 +7,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 @RestController
 @RequestMapping("/api/photos")
 public class PhotoController {
 
-    @GetMapping("/get-photo")
-    public ResponseEntity<Resource> getLostPetPhoto(@RequestBody PhotoGetDTO photoGetDTO) {
-        Resource resource = new FileSystemResource(photoGetDTO.getPhotoPath());
-        if (resource.exists()) {
-            HttpHeaders headers = new HttpHeaders();
+    @GetMapping("/get-photo/{photoPath}")
+    public ResponseEntity<Resource> getLostPetPhoto(@PathVariable String photoPath) {
+        try {
+            String decodedPath = URLDecoder.decode(photoPath, "UTF-8");
+            Resource resource = new FileSystemResource(photoPath);
+            if (resource.exists()) {
+                HttpHeaders headers = new HttpHeaders();
 
-            MediaType mediaType = null;
-            if (photoGetDTO.getPhotoPath().substring(photoGetDTO.getPhotoPath().lastIndexOf(".")+1).equals("jpg") ||
-                    photoGetDTO.getPhotoPath().substring(photoGetDTO.getPhotoPath().lastIndexOf(".")+1).equals("jpeg")){
-                mediaType = MediaType.IMAGE_JPEG;
-            }
-            if (photoGetDTO.getPhotoPath().substring(photoGetDTO.getPhotoPath().lastIndexOf(".")+1).equals("png")){
-                mediaType = MediaType.IMAGE_PNG;
-            }
-            headers.setContentType(mediaType);
+                MediaType mediaType = null;
+                if (decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("jpg") ||
+                        decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("jpeg")) {
+                    mediaType = MediaType.IMAGE_JPEG;
+                }
+                if (decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("png")) {
+                    mediaType = MediaType.IMAGE_PNG;
+                }
+                headers.setContentType(mediaType);
 
-            return new ResponseEntity<>(resource, headers, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (UnsupportedEncodingException ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
