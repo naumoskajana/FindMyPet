8977192ecb27f085bc12da2ba0c3ed279a7ff5bb
jana.naumoska <jana.naumoska@scalefocus.com>
2023-07-05 14:56:23 +0200

    Modified get-photo endpoint to return bytes instead of resource

--- src/main/java/com/example/findmypet/web/PhotoController.java
+++ src/main/java/com/example/findmypet/web/PhotoController.java
@@ -1,7 +1,5 @@
 package com.example.findmypet.web;
 
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,36 +9,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/photos")
 public class PhotoController {
 
     @GetMapping("/get-photo")
-    public ResponseEntity<Resource> getLostPetPhoto(@RequestParam String photoPath) {
+    public ResponseEntity<byte[]> getLostPetPhoto(@RequestParam String photoPath) {
         try {
-            String decodedPath = URLDecoder.decode(photoPath, "UTF-8");
-            Resource resource = new FileSystemResource(decodedPath);
-            if (resource.exists()) {
-                HttpHeaders headers = new HttpHeaders();
+            String decodedPath = URLDecoder.decode(photoPath, StandardCharsets.UTF_8.toString());
+            Path absolutePath = Paths.get(decodedPath).toAbsolutePath();
+            byte[] photoBytes = Files.readAllBytes(absolutePath);
 
-                MediaType mediaType = null;
-                if (decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("jpg") ||
-                        decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("jpeg")) {
-                    mediaType = MediaType.IMAGE_JPEG;
-                }
-                if (decodedPath.substring(decodedPath.lastIndexOf(".") + 1).equals("png")) {
-                    mediaType = MediaType.IMAGE_PNG;
-                }
-                headers.setContentType(mediaType);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
 
-                return new ResponseEntity<>(resource, headers, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (UnsupportedEncodingException ex){
+            return new ResponseEntity<>(photoBytes, headers, HttpStatus.OK);
+        } catch (IOException ex) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
